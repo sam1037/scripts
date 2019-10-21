a974c8ca7542d4abe8a4a3d91634db42d8116d1e
sam1037 <39227131+sam1037@users.noreply.github.com>
2019-10-21 21:56:03 +0800

    changed bookmarking from using url to use clicking, and added change tab hotkey

--- chrome_hotkeys.py
+++ chrome_hotkeys.py
@@ -8,11 +8,14 @@ from photkey_script_funcs import *
 current_keys = set()
 
 
-hotkeys_to_funcs={frozenset([KeyCode(char='`'), KeyCode(char='b')]): [delete_key_twice, bookmark_process]
+hotkeys_to_funcs={frozenset([KeyCode(char='`'), KeyCode(char='b')]): [delete_key_twice, bookmark_process],
+                  frozenset([Key.tab, Key.left]): [change_web_tab_to_left],  # todo fix not able to use it continuously
+                  frozenset([Key.tab, Key.right]): [change_web_tab_to_right],
                   }
 
 
 def on_press(key):
+    print('\nKey {0} pressed'.format(key))
     current_keys.add(key)  # add current key for checking
     #print(current_keys)
     if frozenset(current_keys) in hotkeys_to_funcs:  # if curtain hotkey is pressed
@@ -20,13 +23,16 @@ def on_press(key):
             func()
 
 
+
 def on_release(key):
+    print('Key {0} released'.format(key))
     current_keys.clear()
 
 
 def listen():
     with keyboard.Listener(on_press=on_press, on_release=on_release) as kl:
         kl.join()
+
 
 def main():
     while True:  # if chrome is opened, activate the script
--- chrome_hotkeys_funcs.py
+++ chrome_hotkeys_funcs.py
@@ -2,28 +2,18 @@ from photkey_script_funcs import *
 from pynput.keyboard import Key, Listener
 
 
-def choose_bookmark(num):
-    if type(num) != int:
-        print('ERROR: input should be int')
-        return
-    global bookmarks
-    # todo update bookmarks automatical constantly
-    bookmarks = {1: 'hello',
-                 2: 'https://www.netflix.com/browse',
-                 3: 'https://github.com/',
-                 4: 'https://www.ticktick.com/#q/all/tasks',
-                 5: 'https://www.evernote.com/client/web?login=true#?an=true&n=23508630-e7c2-4132-98ac-43b8e246c2e0&s'
-                    '=s570&',
-                 6: 'https://stackoverflow.com/'}  # bookmarknum to url
+def choose_bookmark(num):  # todo fix not working in not full screen
+    import pyautogui as pg
 
-    pg.hotkey('ctrl', 't')
-    time.sleep(0.1)
-    pg.typewrite(bookmarks[num])  # todo fix why alaways type slowly except for a few times after using after a while
-    pg.hotkey('enter')
+    pg.click(1901, 53, button='left')
+
+    pg.click(1840, 211, button='left')
+
+    pg.click(1460, 345 + (num - 1) * 22, button='left')
 
 def bookmark_process():  # take a number and open certain bookmark
     time.sleep(0.1)
-    print('---------------------------')
+    print('-----------------------------------')
     print('processing')
     print("listening for keystroke")
 
@@ -43,11 +33,21 @@ def bookmark_process():  # take a number and open certain bookmark
             choose_bookmark(num=key)
 
         finally:
-            print('End with {0} key pressed\n-----------------------------------'.format(key))
+            print('End with key {0} pressed\n-----------------------------------'.format(key))
             return False  # end listening, end process
 
     with Listener(
             on_press=on_press,
             on_release=on_release) as bookmark_listener:
         bookmark_listener.join()
+
+
+def change_web_tab_to_left():
+    print('change web tab to left')
+    pg.hotkey('ctrl', 'shift', 'tab')
+
+
+def change_web_tab_to_right():
+    print('change web tab to right')
+    pg.hotkey('ctrl', 'tab')
 
