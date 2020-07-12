780bdb3720591b4038aa4ca1ee4226f73a94dc3a
sam1037 <samngai2522@gmail.com>
2020-07-12 21:19:25 +0800

    I fixed everything, I AM GOD! Just kidding, the pyautogui module does not work for some reason, turns out it need pywinauto==0.6.6 instead of 0.67 for some god know reason. The whole begining is that the python in this venv is too old to install inspect module

--- test3.py
+++ test3.py
@@ -1,1 +1,25 @@
-import pywin32_postinstall
+from pynput import keyboard
+from pynput.keyboard import KeyCode, Key
+from photkey_script_funcs import delete_key_once
+
+
+def deleteKeyOnce():
+    import pyautogui as pg
+    pg.press("backspace")
+
+def on_press(key):
+    print(f"{key} key pressed")
+    if not key == Key.backspace:
+        deleteKeyOncea()
+
+def on_release(key):
+    print(f"{key} key released")
+
+def listen():
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as k:
+        k.join()
+
+import pyautogui
+import time
+time.sleep(3)
+pyautogui.press("backspace")
